78acf6466753572550f0e6fb83edf41b19086825
Vinícius Simões <vinisv2004@gmail.com>
2026-05-03 08:29:47 -0300

    ajustes para hospedar o backend no render

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,12 @@
 // js/config.js
+
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
 export const CONFIG = {
-    API_URL: 'http://localhost:8080',
+    // Detecta se deve usar o backend local ou o do Render
+    API_URL: isLocalhost 
+        ? 'http://localhost:8080' 
+        : 'https://seu-projeto-backend.onrender.com',
     SUPABASE_URL: 'https://logixvrbnxyuklwwouad.supabase.co/',
     SUPABASE_KEY: 'sb_publishable_hTOjl9pkgv_KwUiOMxw3mA_LY9wxc4c'
 };
