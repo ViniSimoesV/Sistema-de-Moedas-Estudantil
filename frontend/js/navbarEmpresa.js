87677984f0d33d6cca0e368a0150acfed1971c3d
Vinícius Simões <vinisv2004@gmail.com>
2026-05-06 00:12:13 -0300

    icons navbar empresa

--- frontend/js/navbarEmpresa.js
+++ frontend/js/navbarEmpresa.js
@@ -5,10 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
         <header class="navbar">
             <div class="logo">Uni<span>Rewards</span></div>
             <nav class="nav-links">
-                <a href="dashboardEmpresa.html" class="nav-link">Início</a>
-                <a href="vantagens.html" class="nav-link">Vantagens</a>
-                <a href="perfilEmpresa.html" class="nav-link active">Perfil</a>
-                <a href="../index.html" class="nav-link">Sair</a>
+                <a href="vantagens.html" class="nav-link">
+                    <span class="material-symbols-outlined">inventory</span>
+                    Vantagens
+                </a>
+                <a href="perfilEmpresa.html" class="nav-link active">
+                    <span class="material-symbols-outlined">person</span>
+                    Perfil
+                </a>
+                <a href="../index.html" class="nav-link">
+                    <span class="material-symbols-outlined">logout</span>
+                    Sair
+                </a>
             </nav>
         </header>
         `;
