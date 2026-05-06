21279a6a4c64067b8df758ee6b22de4724fe4096
Vinícius Simões <vinisv2004@gmail.com>
2026-05-06 00:01:48 -0300

    Ajustes navbar mobile

--- frontend/js/navbarAluno.js
+++ frontend/js/navbarAluno.js
@@ -5,10 +5,22 @@ document.addEventListener("DOMContentLoaded", () => {
         <header class="navbar">
             <div class="logo">Uni<span>Rewards</span></div>
             <nav class="nav-links">
-                <a href="dashboard.html" class="nav-link">Home</a>
-                <a href="loja.html" class="nav-link">Loja</a>
-                <a href="alunoPerfil.html" class="nav-link active">Perfil</a>
-                <a href="../index.html" class="nav-link">Sair</a>
+                <a href="loja.html" class="nav-link">
+                    <span class="material-symbols-outlined">shopping_bag</span>
+                    Loja
+                </a>
+                <a href="inventario.html" class="nav-link">
+                    <span class="material-symbols-outlined">inventory</span>
+                    Inventário
+                </a>
+                <a href="alunoPerfil.html" class="nav-link active">
+                    <span class="material-symbols-outlined">person</span>
+                    Perfil
+                </a>
+                <a href="../index.html" class="nav-link">
+                    <span class="material-symbols-outlined">exit_to_app</span>
+                    Sair
+                </a>
             </nav>
         </header>
         `;
